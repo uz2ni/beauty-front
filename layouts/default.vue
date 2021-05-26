b34b31f5cf8c9be4f3c0a9c7a1c7f13eee773ccb
uz2ni <uzini_@likelion.org>
2021-05-26 23:34:54 +0900

    [레이아웃] 상단바 구조,색상 변경

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,5 +1,5 @@
 <template>
-  <v-app dark>
+  <v-app light>
     <v-navigation-drawer
       v-model="drawer"
       :mini-variant="miniVariant"
@@ -24,61 +24,38 @@
         </v-list-item>
       </v-list>
     </v-navigation-drawer>
-    <v-app-bar
-      :clipped-left="clipped"
-      fixed
-      app
-    >
-      <v-app-bar-nav-icon @click.stop="drawer = !drawer" />
-      <v-btn
-        icon
-        @click.stop="miniVariant = !miniVariant"
-      >
-        <v-icon>mdi-{{ `chevron-${miniVariant ? 'right' : 'left'}` }}</v-icon>
-      </v-btn>
-      <v-btn
-        icon
-        @click.stop="clipped = !clipped"
-      >
-        <v-icon>mdi-application</v-icon>
-      </v-btn>
-      <v-btn
-        icon
-        @click.stop="fixed = !fixed"
-      >
-        <v-icon>mdi-minus</v-icon>
-      </v-btn>
-      <v-toolbar-title v-text="title" />
-      <v-spacer />
-      <v-btn
-        icon
-        @click.stop="rightDrawer = !rightDrawer"
-      >
-        <v-icon>mdi-menu</v-icon>
-      </v-btn>
-    </v-app-bar>
+
+
+        <v-app-bar
+          class="pa-0"
+          color="blue darken-4"
+          :clipped-left="clipped"
+          fixed
+          app
+        >
+          <v-container class="ma-0 pa-0">
+            <v-row
+              no-gutters
+            >
+              <v-col cols="4">
+                <v-app-bar-nav-icon @click.stop="drawer = !drawer" color="white"/>
+              </v-col>
+
+              <v-col cols="4" class="d-flex align-center justify-center">
+                <v-toolbar-title v-text="title" class="text-center" style="color: white;"/>
+              </v-col>
+
+              <v-col cols="4">
+              </v-col>
+            </v-row>
+          </v-container>
+        </v-app-bar>
+
     <v-main>
       <v-container>
         <nuxt />
       </v-container>
     </v-main>
-    <v-navigation-drawer
-      v-model="rightDrawer"
-      :right="right"
-      temporary
-      fixed
-    >
-      <v-list>
-        <v-list-item @click.native="right = !right">
-          <v-list-item-action>
-            <v-icon light>
-              mdi-repeat
-            </v-icon>
-          </v-list-item-action>
-          <v-list-item-title>Switch drawer (click me)</v-list-item-title>
-        </v-list-item>
-      </v-list>
-    </v-navigation-drawer>
     <v-footer
       :absolute="!fixed"
       app
@@ -98,19 +75,29 @@ export default {
       items: [
         {
           icon: 'mdi-apps',
-          title: 'Welcome',
+          title: '홈',
           to: '/'
         },
         {
           icon: 'mdi-chart-bubble',
-          title: 'Inspire',
+          title: '체험단',
           to: '/inspire'
+        },
+        {
+          icon: 'mdi-chart-bubble',
+          title: 'NEW',
+          to: '/inspire2'
+        },
+        {
+          icon: 'mdi-chart-bubble',
+          title: 'EVENT',
+          to: '/inspire3'
         }
       ],
       miniVariant: false,
       right: true,
       rightDrawer: false,
-      title: 'Vuetify.js'
+      title: 'BEAUTY'
     }
   }
 }
