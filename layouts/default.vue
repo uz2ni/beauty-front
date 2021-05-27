4fde994cd261847e17f10608803ef4a5cc32e475
uz2ni <uzini_@likelion.org>
2021-05-27 22:04:31 +0900

    [레이아웃] 탭, 홈 더미용 카드 추가

--- layouts/default.vue
+++ layouts/default.vue
@@ -26,42 +26,93 @@
     </v-navigation-drawer>
 
 
-        <v-app-bar
-          class="pa-0"
-          color="blue darken-4"
-          :clipped-left="clipped"
-          fixed
-          app
+    <v-app-bar
+      class="pa-0"
+      color="blue darken-4"
+      :clipped-left="clipped"
+      fixed
+      app
+    >
+      <v-container class="ma-0 pa-0">
+        <v-row
+          no-gutters
         >
-          <v-container class="ma-0 pa-0">
-            <v-row
-              no-gutters
-            >
-              <v-col cols="4">
-                <v-app-bar-nav-icon @click.stop="drawer = !drawer" color="white"/>
-              </v-col>
+          <v-col cols="4">
+            <v-app-bar-nav-icon @click.stop="drawer = !drawer" color="white"/>
+          </v-col>
 
-              <v-col cols="4" class="d-flex align-center justify-center">
-                <v-toolbar-title v-text="title" class="text-center" style="color: white;"/>
-              </v-col>
+          <v-col cols="4" class="d-flex align-center justify-center">
+            <v-toolbar-title v-text="title" class="text-center" style="color: white;"/>
+          </v-col>
 
-              <v-col cols="4">
-              </v-col>
-            </v-row>
-          </v-container>
-        </v-app-bar>
+          <v-col cols="4">
+          </v-col>
+        </v-row>
+      </v-container>
+
+      <template v-slot:extension>
+        <v-tabs
+          class="tab-menu"
+          fixed-tabs
+          centered
+          slider-color="white"
+        >
+          <v-tab href="/tab1" class="white--text">홈</v-tab>
+          <v-tab href="/tab2" class="white--text">질문</v-tab>
+          <v-tab href="/tab3" class="white--text">리뷰</v-tab>
+        </v-tabs>
+      </template>
+    </v-app-bar>
+
 
     <v-main>
-      <v-container>
+      <v-container class="pb-16">
         <nuxt />
       </v-container>
     </v-main>
-    <v-footer
-      :absolute="!fixed"
+    <!--<v-footer
       app
+      class="pb-16"
     >
       <span>&copy; {{ new Date().getFullYear() }}</span>
-    </v-footer>
+    </v-footer>-->
+
+    <v-bottom-navigation
+      fixed
+      v-model="value"
+      color="blue darken-4"
+    >
+      <v-btn>
+        <span>홈</span>
+
+        <v-icon>mdi-home</v-icon>
+      </v-btn>
+
+      <v-btn>
+        <span>이벤트</span>
+
+        <v-icon>mdi-gift-outline</v-icon>
+      </v-btn>
+
+      <v-btn>
+        <span>포스트</span>
+
+        <v-icon>mdi-text-box-outline</v-icon>
+      </v-btn>
+
+      <v-btn>
+        <span>스토어</span>
+
+        <v-icon>mdi-store</v-icon>
+      </v-btn>
+
+      <v-btn>
+        <span>MY</span>
+
+        <v-icon>mdi-account-box</v-icon>
+      </v-btn>
+    </v-bottom-navigation>
+
   </v-app>
 </template>
 
@@ -86,19 +137,22 @@ export default {
         {
           icon: 'mdi-chart-bubble',
           title: 'NEW',
-          to: '/inspire2'
+          to: '/inspire'
         },
         {
           icon: 'mdi-chart-bubble',
           title: 'EVENT',
-          to: '/inspire3'
+          to: '/inspire'
         }
       ],
       miniVariant: false,
       right: true,
       rightDrawer: false,
-      title: 'BEAUTY'
+      title: 'BEAUTY',
+      value: 1
     }
   }
 }
 </script>
+<style scoped>
+</style>
---
+++ pages/tab1.vue
@@ -0,0 +1,111 @@
+<template>
+  <v-container fluid>
+    <v-row dense>
+      <v-col
+        v-for="card in cards"
+        :key="card.title"
+        :cols="card.flex"
+      >
+        <v-card>
+          <v-img
+            :src="card.src"
+            class="white--text align-end"
+            gradient="to bottom, rgba(0,0,0,.1), rgba(0,0,0,.5)"
+            height="200px"
+          >
+            <v-card-title v-text="card.title"></v-card-title>
+          </v-img>
+
+          <v-card-actions>
+            <v-spacer></v-spacer>
+
+            <v-btn icon>
+              <v-icon>mdi-heart</v-icon>
+            </v-btn>
+
+            <v-btn icon>
+              <v-icon>mdi-bookmark</v-icon>
+            </v-btn>
+
+            <v-btn icon>
+              <v-icon>mdi-share-variant</v-icon>
+            </v-btn>
+          </v-card-actions>
+        </v-card>
+      </v-col>
+    </v-row>
+
+    <v-row>
+      <v-col>
+        <v-card
+          class="mx-auto"
+          color="#26c6da"
+          dark
+          max-width="400"
+        >
+          <v-card-title>
+            <v-icon
+              large
+              left
+            >
+              mdi-twitter
+            </v-icon>
+            <span class="title font-weight-light">Twitter</span>
+          </v-card-title>
+
+          <v-card-text class="headline font-weight-bold">
+            "Turns out semicolon-less style is easier and safer in TS because most gotcha edge cases are type invalid as well."
+          </v-card-text>
+
+          <v-card-actions>
+            <v-list-item class="grow">
+              <v-list-item-avatar color="grey darken-3">
+                <v-img
+                  class="elevation-6"
+                  alt=""
+                  src="https://avataaars.io/?avatarStyle=Transparent&topType=ShortHairShortCurly&accessoriesType=Prescription02&hairColor=Black&facialHairType=Blank&clotheType=Hoodie&clotheColor=White&eyeType=Default&eyebrowType=DefaultNatural&mouthType=Default&skinColor=Light"
+                ></v-img>
+              </v-list-item-avatar>
+
+              <v-list-item-content>
+                <v-list-item-title>Evan You</v-list-item-title>
+              </v-list-item-content>
+
+              <v-row
+                align="center"
+                justify="end"
+              >
+                <v-icon class="mr-1">
+                  mdi-heart
+                </v-icon>
+                <span class="subheading mr-2">256</span>
+                <span class="mr-1">·</span>
+                <v-icon class="mr-1">
+                  mdi-share-variant
+                </v-icon>
+                <span class="subheading">45</span>
+              </v-row>
+            </v-list-item>
+          </v-card-actions>
+        </v-card>
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      cards: [
+        { title: 'Pre-fab homes', src: 'https://cdn.vuetifyjs.com/images/cards/house.jpg', flex: 12 },
+        { title: 'Favorite road trips', src: 'https://cdn.vuetifyjs.com/images/cards/road.jpg', flex: 6 },
+        { title: 'Best airlines', src: 'https://cdn.vuetifyjs.com/images/cards/plane.jpg', flex: 6 },
+        { title: 'Pre-fab homes', src: 'https://cdn.vuetifyjs.com/images/cards/house.jpg', flex: 12 },
+        { title: 'Favorite road trips', src: 'https://cdn.vuetifyjs.com/images/cards/road.jpg', flex: 6 },
+        { title: 'Best airlines', src: 'https://cdn.vuetifyjs.com/images/cards/plane.jpg', flex: 6 }
+      ]
+    }
+  }
+}
+</script>
