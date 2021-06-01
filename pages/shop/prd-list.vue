bd2c3081756a1f4355956218adf4afcbe8942262
uz2ni <uzini_@likelion.org>
2021-06-01 23:29:47 +0900

    SHOP 탭 목록 구조 추가

---
+++ pages/shop/prd-list.vue
@@ -0,0 +1,13 @@
+<template>
+<div>크롤링 데이터 뿌리기</div>
+</template>
+
+<script>
+export default {
+  name: "brand"
+}
+</script>
+
+<style scoped>
+
+</style>
--- pages/tab4.vue
+++ pages/tab4.vue
@@ -1,19 +1,45 @@
 <template>
   <v-row>
-    <v-col class="text-center">
-      <img
-        src="/v.png"
-        alt="Vuetify.js"
-        class="mb-5"
-      >
-      <blockquote class="blockquote">
-        &#8220;new&#8221;
-        <footer>
-          <small>
-            <em>&mdash;John Johnson</em>
-          </small>
-        </footer>
-      </blockquote>
+    <v-col>
+      <v-list>
+        <nuxt-link to="/shop/prd-list">
+          <v-list-item
+            v-for="item in items"
+            :key="item.title"
+          >
+            <v-list-item-icon>
+              <v-icon
+                v-if="item.icon"
+                color="pink"
+              >
+                mdi-star
+              </v-icon>
+            </v-list-item-icon>
+
+            <v-list-item-content>
+              <v-list-item-title v-text="item.title"></v-list-item-title>
+            </v-list-item-content>
+
+            <v-list-item-avatar>
+              <v-img :src="item.avatar"></v-img>
+            </v-list-item-avatar>
+          </v-list-item>
+        </nuxt-link>
+      </v-list>
     </v-col>
   </v-row>
 </template>
+<script>
+export default {
+  data() {
+    return {
+      items: [
+        {icon: true, title: 'Papa Receipe', avatar: '/images/brand-logo/papa-recipe.png'},
+        {title: 'Travis Howard', avatar: 'https://cdn.vuetifyjs.com/images/lists/2.jpg'},
+        {title: 'Ali Connors', avatar: 'https://cdn.vuetifyjs.com/images/lists/3.jpg'},
+        {title: 'Cindy Baker', avatar: 'https://cdn.vuetifyjs.com/images/lists/4.jpg'},
+      ]
+    }
+  }
+}
+</script>
