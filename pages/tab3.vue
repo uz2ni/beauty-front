dc10e2b8117b209dba9391bbe128f72adabaac0c
uz2ni <uzini_@likelion.org>
2021-06-14 23:15:46 +0900

    [가격배너] tab3 배너 설계

--- pages/tab3.vue
+++ pages/tab3.vue
@@ -1,19 +1,37 @@
 <template>
-  <v-row>
-    <v-col class="text-center">
-      <img
-        src="/v.png"
-        alt="Vuetify.js"
-        class="mb-5"
+  <v-container fluid>
+    <v-row dense>
+      <v-col
+        v-for="banner in banners"
+        :key="banner.id"
+        :cols="12"
       >
-      <blockquote class="blockquote">
-        &#8220;tip&#8221;
-        <footer>
-          <small>
-            <em>흠....</em>
-          </small>
-        </footer>
-      </blockquote>
-    </v-col>
-  </v-row>
+        <a :href="banner.landingUrl"><v-card class="my-1">
+          <v-img
+            src="/images/price-banner-01.png"
+            class="white--text align-center"
+            height="100px"
+          >
+            <v-card-title v-text="banner.title" class="card-tit font-weight-bold"></v-card-title>
+            <v-card-subtitle class="font-weight-bold">{{ banner.price }}원</v-card-subtitle>
+          </v-img>
+        </v-card></a>
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
+
+<script>
+export default {
+  async asyncData ({ app, params }) {
+    const { data } = await app.$axios.$get('price-banner')
+    return { banners: data.priceBanners }
+  }
+}
+</script>
+<style scoped>
+.card-tit {
+  font-size: 0.9rem;
+  line-height: 1rem;
+}
+</style>
