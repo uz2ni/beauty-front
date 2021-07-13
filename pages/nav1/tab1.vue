9388da324f3536eca8699dab1ceddf95bb804ebf
uz2ni <uzini_@likelion.org>
2021-07-13 18:23:16 +0900

    제품별>전체 탭 상품 좋아요 토글 적용

--- pages/nav1/tab1.vue
+++ pages/nav1/tab1.vue
@@ -3,8 +3,8 @@
     <v-row style="height:20vh; background-color: gray;">배너 영역</v-row>
     <v-row class="pa-2">
       <v-col
-        v-for="(banner, index) in banners"
-        :key="index"
+        v-for="(product, index) in products"
+        :key="product.id"
         cols="6"
         :class="cardPadding(index)"
       >
@@ -20,17 +20,17 @@
           >
             <v-card-actions>
               <v-spacer></v-spacer>
-              <v-btn icon>
-                <v-icon color="white">mdi-heart-outline</v-icon>
+              <v-btn icon @click="updateLike(product.id)">
+                <v-icon :color="likeColor(product.id)">{{ likeToggle(product.id) }}</v-icon>
               </v-btn>
             </v-card-actions>
           </v-img>
         </v-card>
         <!-- product info -->
         <div class="prd-info-wrap">
-          <div class="prd-shopnm">{{ banner.shopName }}</div>
-          <div class="prd-title">{{ banner.title }}</div>
-          <div class="prd-price">{{ banner.price | comma }}</div>
+          <div class="prd-shopnm">{{ product.shopName }}</div>
+          <div class="prd-title">{{ product.title }}</div>
+          <div class="prd-price">{{ product.price | comma }}</div>
         </div>
 
       </v-col>
@@ -42,38 +42,45 @@
 export default {
   data() {
     return {
-      banners: [
+      likeProduct: [1,2], // product id
+      products: [
         {
+          "id": 1,
           "title": "그린 마일드 업 선 플러스 50ml",
           "price": 21000,
           "landingUrl": "https://www.dr-g.co.kr/shop/goods/goods_view.php?goodsno=3797&category=014007",
           "shopName" : "닥터지"
         },
         {
+          "id": 2,
           "title": "노세범 미네랄 파우더 + 노세범 픽싱 피니시 파우더 5g",
           "price": 15400,
           "landingUrl": "https://www.innisfree.com/kr/ko/ProductView.do?prdSeq=29996&catCd01=&catCd02=&tp=",
           "shopName" : "이니스프리"
         },
         {
+          "id": 3,
           "title": "모레모 헤어 트리트먼트 미라클 2X 480ML 대용량",
           "price": 27300,
           "landingUrl": "https://moremofam.co.kr/product/%EB%AA%A8%EB%A0%88%EB%AA%A8-%ED%97%A4%EC%96%B4-%ED%8A%B8%EB%A6%AC%ED%8A%B8%EB%A8%BC%ED%8A%B8-%EB%AF%B8%EB%9D%BC%ED%81%B4-2x-%EB%9F%AC%EB%B8%8C-%EC%97%90%EB%94%94%EC%85%98-480ml-%EB%8C%80%EC%9A%A9%EB%9F%89/80/category/24/display/1/",
           "shopName" : "모레모"
         },
         {
+          "id": 4,
           "title": "그린 마일드 업 선 플러스 50ml",
           "price": 21000,
           "landingUrl": "https://www.dr-g.co.kr/shop/goods/goods_view.php?goodsno=3797&category=014007",
           "shopName" : "닥터지"
         },
         {
+          "id": 5,
           "title": "노세범 미네랄 파우더 + 노세범 픽싱 피니시 파우더 5g",
           "price": 15400,
           "landingUrl": "https://www.innisfree.com/kr/ko/ProductView.do?prdSeq=29996&catCd01=&catCd02=&tp=",
           "shopName" : "이니스프리"
         },
         {
+          "id": 6,
           "title": "모레모 헤어 트리트먼트 미라클 2X 480ML 대용량",
           "price": 27300,
           "landingUrl": "https://moremofam.co.kr/product/%EB%AA%A8%EB%A0%88%EB%AA%A8-%ED%97%A4%EC%96%B4-%ED%8A%B8%EB%A6%AC%ED%8A%B8%EB%A8%BC%ED%8A%B8-%EB%AF%B8%EB%9D%BC%ED%81%B4-2x-%EB%9F%AC%EB%B8%8C-%EC%97%90%EB%94%94%EC%85%98-480ml-%EB%8C%80%EC%9A%A9%EB%9F%89/80/category/24/display/1/",
@@ -87,6 +94,18 @@ export default {
       return (index) => {
         return (index%2 == 0) ? 'pr-2' : 'pl-2';
       }
+    },
+    likeColor() {
+      return (id) => {
+        const productId = this.likeProduct.find((value) => value == id);
+        return (productId === undefined) ? 'white' : 'primary';
+      }
+    },
+    likeToggle() {
+      return (id) => {
+        const productId = this.likeProduct.find((value) => value == id);
+        return (productId === undefined) ? 'mdi-heart-outline' : 'mdi-heart';
+      }
     }
   },
   filters:{
@@ -97,6 +116,15 @@ export default {
   methods: {
     imgUrlSetting (id) { // 배너 이미지 임시용
       return `/images/banner${id%3+1}.jpg`;
+    },
+    updateLike(id) {
+      const productId = this.likeProduct.find((value) => value == id);
+      if(productId === undefined) {
+        this.likeProduct.push(id);
+      }else {
+        const idx = this.likeProduct.indexOf(productId);
+        if (idx > -1) this.likeProduct.splice(idx, 1);
+      }
     }
   }
 }
