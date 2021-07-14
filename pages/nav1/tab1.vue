a3a8c2b92304e84b26791afac6fa7cbf9f49ac26
uz2ni <uzini_@likelion.org>
2021-07-14 16:11:24 +0900

    제품별>전체 탭 상단배너 적용

--- pages/nav1/tab1.vue
+++ pages/nav1/tab1.vue
@@ -1,6 +1,21 @@
 <template>
   <div>
-    <v-row style="height:20vh; background-color: gray;">배너 영역</v-row>
+    <v-row>
+      <v-carousel
+        cycle
+        height="25vh"
+        hide-delimiter-background
+        :show-arrows="false"
+      >
+        <v-carousel-item
+          v-for="(banner, i) in banners"
+          :key="i"
+          :src="banner.src"
+        >
+        </v-carousel-item>
+      </v-carousel>
+    </v-row>
+
     <v-row class="pa-2">
       <v-col
         v-for="(product, index) in products"
@@ -42,6 +57,25 @@
 export default {
   data() {
     return {
+      banners: [
+        {
+          "id" : 1,
+          "title" : "banner1",
+          "src" :"/images/price-banner-01(375x203).png",
+          "landingUrl": "https://www.dr-g.co.kr/shop/goods/goods_view.php?goodsno=3797&category=014007"
+        },
+        {
+          "id" : 2,
+          "title" : "banner2",
+          "src" :"/images/price-banner-02(375x203).png",
+          "landingUrl": "https://www.innisfree.com/kr/ko/ProductView.do?prdSeq=29996&catCd01=&catCd02=&tp="
+        },{
+          "id" : 3,
+          "title" : "banner3",
+          "src" :"/images/price-banner-03(375x203).png",
+          "landingUrl": "https://moremofam.co.kr/product/%EB%AA%A8%EB%A0%88%EB%AA%A8-%ED%97%A4%EC%96%B4-%ED%8A%B8%EB%A6%AC%ED%8A%B8%EB%A8%BC%ED%8A%B8-%EB%AF%B8%EB%9D%BC%ED%81%B4-2x-%EB%9F%AC%EB%B8%8C-%EC%97%90%EB%94%94%EC%85%98-480ml-%EB%8C%80%EC%9A%A9%EB%9F%89/80/category/24/display/1/"
+        }
+      ],
       likeProduct: [1,2], // product id
       products: [
         {
@@ -143,6 +177,15 @@ export default {
   font-weight: bold;
   color: black;
   font-size:0.8rem;
+}
+/* slide banner custom */
+::v-deep .v-carousel__controls > div > button{
+  margin: 0;
+  width: 17px;
+  height: 17px;
+}
+::v-deep .v-carousel__controls > div > button > span > i {
+  font-size: 10px !important;
 }
 
 
