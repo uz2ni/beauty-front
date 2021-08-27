01360d916c9156e29c5300fabe4ad303bea981b1
uz2ni <uzini_@likelion.org>
2021-08-27 21:24:36 +0900

    MY - 프로필 상단 생성중

--- pages/nav5/tab1.vue
+++ pages/nav5/tab1.vue
@@ -1,5 +1,34 @@
 <template>
-  <div>마이페이지</div>
+  <div>
+    <div>
+      <v-card-title>
+        <v-avatar size="56">
+          <img
+            alt="user"
+            src="https://cdn.pixabay.com/photo/2020/06/24/19/12/cabbage-5337431_1280.jpg"
+          >
+        </v-avatar>
+        <div class="profile-info">
+          <div class="profile-nickname">유지니</div>
+          <div class="profile-id">@uz2ni__</div>
+        </div>
+        <div class="follow-wrap">
+          <v-btn
+            rounded
+            color="primary"
+          >
+            팔로우
+          </v-btn>
+<!--          <v-btn-->
+<!--            rounded-->
+<!--            color="secondary"-->
+<!--          >-->
+<!--            팔로잉-->
+<!--          </v-btn>-->
+        </div>
+      </v-card-title>
+    </div>
+  </div>
 </template>
 
 <script>
@@ -9,5 +38,22 @@ export default {
 </script>
 
 <style scoped>
-
+.profile-nickname {
+  font-size: 1.5rem;
+}
+.profile-id {
+  font-size: 0.8rem;
+  font-weight: initial;
+}
+::v-deep .v-card__title {
+  line-height: 1.5rem;
+  padding: 0;
+}
+.profile-info {
+  margin-left: 10px;
+}
+.follow-wrap {
+  position: absolute;
+  right: 15px;
+}
 </style>
